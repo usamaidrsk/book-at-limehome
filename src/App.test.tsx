41ca212c62aa6d27079280e3674661b5b0b83bbd
Lutaaya Idris <lutaaya.idris@captiq.com>
2022-04-05 13:27:55 +0300

    Add tests, README file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { mount } from '@cypress/react';
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Make reservation flow', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('renders title of the page', () => {
+    mount(<App />);
+    cy.get('h2').contains('Booking your suite at limehouse');
+  });
+
+  it('should make a reservation', ()=> {
+    cy.get('[name="numberOfGuests]').first().type('3').blur()
+    cy.get('[name="personalDetails.firstName"]').first().type('john').blur()
+    cy.get('[name="personalDetails.lastName"]').first().type('john').blur()
+    cy.get('[name="location.billingAddress"]').first().type('john').blur()
+  })
+})
